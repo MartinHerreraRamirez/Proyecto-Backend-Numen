00f153451d03480b7d6e2da010146af8b622529d
MartinHerreraRamirez <martin.herrera.ramirez.2016@gmail.com>
2022-10-03 12:03:26 -0300

    se agregan validaciones

--- middleware/validatePlayer.js
+++ middleware/validatePlayer.js
@@ -1,9 +1,9 @@
 const {check, validationResult, body} = require('express-validator')
 
 exports.validatePlayer = [
-    check('name').not().isEmpty().withMessage('The field is empty').isLength({max:25, min:3}).withMessage('The Name must have between 3 and 25 characters'),
-    check('surname').not().isEmpty().withMessage('The field is empty').isLength({max:25, min:3}).withMessage('The Surname must have between 3 and 25 characters'),
-    check('shirtNumber').not().isEmpty().withMessage('The field is empty').isLength({max:3, min:1}).withMessage('The Shirt Number must have between 1 and 3 numbers'),
+    check('name').not().isEmpty().withMessage('The field is empty').isString().withMessage('The field accepts only characters').isLength({max:25, min:3}).withMessage('The Name must have between 3 and 25 characters'),
+    check('surname').not().isEmpty().withMessage('The field is empty').isString().withMessage('The field accepts only characters').isLength({max:25, min:3}).withMessage('The Surname must have between 3 and 25 characters'),
+    check('shirtNumber').not().isEmpty().withMessage('The field is empty').isNumeric().withMessage('The field accepts only numbers').isLength({max:3, min:1}).withMessage('The Shirt Number must have between 1 and 3 numbers'),
     check('team').not().isEmpty().withMessage('The field is empty').isLength({max:15, min:7}).withMessage('The Team must have between 7 and 15 characters')       
 ]
 
